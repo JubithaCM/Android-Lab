714a07d26d84a15b4ee291cffb271aae382cc9a3
JubithaCM <jubithamohammed@gmail.com>
2022-09-22 11:48:17 +0530

    custom toast registration form

---
+++ Registrationusingcustomtoast/app/src/main/java/com/example/registrationusingcustomtoast/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.registrationusingcustomtoast;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.login).setOnClickListener(view -> {
+            Toast toast = new Toast(this);
+            toast.setView(LayoutInflater.from(this).inflate(R.layout.toast,null));
+            toast.show();
+        });
+    }
+}
