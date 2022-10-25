312a28f1d1cd33611ed85d50076118a70c20526f
JubithaCM <jubithamohammed@gmail.com>
2022-10-25 10:43:28 +0530

    facebook design

--- Facebook/app/src/main/java/com/example/facebook/MainActivity.java
+++ Facebook/app/src/main/java/com/example/facebook/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.facebook;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.save).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = new Toast(getApplicationContext());
+                toast.setView(LayoutInflater.from(getApplicationContext()).inflate(R.layout.activity_toast,null));
+                toast.show();
+            }
+        });
+
     }
 }
+
+
+
